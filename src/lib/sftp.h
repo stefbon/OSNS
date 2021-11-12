4a41906cb80265c14a625afafc92286e5720036e
Stef Bon <stefbon@gmail.com>
2021-11-12 16:28:59 +0100

    Add general header for library header files for sftp.

---
+++ src/lib/sftp.h
@@ -0,0 +1,35 @@
+/*
+  2017 Stef Bon <stefbon@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#ifndef _LIB_SFTP_H
+#define _LIB_SFTP_H
+
+#include "sftp/common-protocol.h"
+#include "sftp/protocol.h"
+
+#include "sftp/attr-context.h"
+
+#include "sftp/attr-init.h"
+#include "sftp/attr-read.h"
+#include "sftp/attr-write.h"
+#include "sftp/attr-utils.h"
+
+#include "sftp/rw-attr-generic.h"
+
+#endif
